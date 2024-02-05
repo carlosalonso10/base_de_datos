f3c7e97f5d60265f48bb49bcd827411527f0e278
PC} <carlosalonsohome@gmail.com>
2024-02-05 15:17:43 -0600

    nuevo actualizacion  5 de febrero actualizacion

--- app/src/main/java/com/example/base_de_datos/MainActivityList.java
+++ app/src/main/java/com/example/base_de_datos/MainActivityList.java
@@ -3,8 +3,15 @@ package com.example.base_de_datos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import Configuracion.SQLiteconexion;
 
 public class MainActivityList extends AppCompatActivity {
+     SQLiteconexion conexion;
+
+     ListView listpersonas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
